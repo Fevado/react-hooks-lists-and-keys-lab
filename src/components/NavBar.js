0ea238c409edbc4f1be2fea5a0fbd21726bd16e5
favoured mwange <mwangefavoured@gmail.com>
2024-08-01 19:48:39 +0300

    Adds the lists and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+const NavLinks = links.map((link)=>{return (<a href={"#link"} key={link}>{link}</a>)})
+  return <nav>{/* display an <a> tag for each link here */}{NavLinks}</nav>;
 }
 
 export default NavBar;
